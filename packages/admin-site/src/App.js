6b1a2b93a80fac3191cb4ee1cb7e472f148616c7
nashdeng <nashdeng@tencent.com>
2021-04-21 15:04:45 +0800

    init

---
+++ packages/admin-site/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Testcom from "@mono-repo-demo/common/components/TestCom";
+function App() {
+  return (
+    <div className="App">
+      <Testcom></Testcom>
+    </div>
+  );
+}
+
+export default App;
